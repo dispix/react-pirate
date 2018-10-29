1f00e4e90a6187cc2652643a325ace4995729c35
Octave Raimbault <me@dispix.net>
2018-10-29 11:11:05 +0100

    chore(package): update bundle
    - add sourcemap
    - main file change from './build/index.cjs.js' to './index.js'

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 
+import packagejson from './package.json'
+
 export default {
   input: 'src/index.js',
   output: {
-    file: 'build/index.cjs.js',
+    file: packagejson.main,
     format: 'cjs',
   },
   plugins: [resolve(), commonjs()],
